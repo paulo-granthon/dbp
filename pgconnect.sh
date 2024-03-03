e59b7aad050c78a310d9ec5abadab9ab5eba2059
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-03-03 01:47:37 -0300

    fix: `pgcli` call in `pgconnect`

--- pgconnect.sh
+++ pgconnect.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 . .env
-db_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' dbp_container_db)
-echo "Connecting to ip \`$db_ip\` with pgcli..."
-pgcli -h $db_ip:$DATABASE_PORT -u $DATABASE_USERNAME -d $DATABASE_NAME
+pgcli postgresql://${DATABASE_USERNAME}:${DATABASE_PASSWORD}@${DATABASE_HOSTNAME}:${DATABASE_PORT}/${DATABASE_NAME}
